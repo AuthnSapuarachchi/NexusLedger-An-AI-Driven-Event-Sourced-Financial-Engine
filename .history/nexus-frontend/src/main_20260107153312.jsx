c9a54402bf0d74416dfd62b3fb24312b6fbc86b9
Authn <authn.sapuarachchi@gmail.com>
2026-01-07 15:52:40 +0530

    all right MVP is setup,

--- .history/nexus-frontend/src/main_20260107153312.jsx
+++ .history/nexus-frontend/src/main_20260107153312.jsx
@@ -5,10 +5,9 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
     <App />
   </StrictMode>,
 )
